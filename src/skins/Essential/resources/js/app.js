9838c90729bc6580e1255efcdb0fb7f6095e0667
Ashley Solomon <ashcslmn@gmail.com>
2021-11-27 09:16:52 +0800

    disable temp quill

--- src/skins/Essential/resources/js/app.js
+++ src/skins/Essential/resources/js/app.js
@@ -1,17 +1,17 @@
 /* eslint-disable */
-$( 'textarea' ).each( function ( i, el ) {
-	var el = $( this ), id = 'quilleditor-' + i, val = el.val(), editor_height = 200,
-		div = $( '<div>' ).attr( 'id', id ).css( 'height', editor_height + 'px' ).html( val );
-	el.addClass( 'hidden' );
-	el.parent().prepend( div );
-	var quill = new Quill( '#' + id, {
-		modules: { 
-            toolbar: true,
-        },
-		theme: 'snow'
-	} );
-	quill.on( 'text-change', function () {
-		// TODO: Sanitize and convert to mediawiki markdown
-        el.html(quill.container.firstChild.innerHTML);
-	} );
-});
+// $( 'textarea' ).each( function ( i, el ) {
+// 	var el = $( this ), id = 'quilleditor-' + i, val = el.val(), editor_height = 200,
+// 		div = $( '<div>' ).attr( 'id', id ).css( 'height', editor_height + 'px' ).html( val );
+// 	el.addClass( 'hidden' );
+// 	el.parent().prepend( div );
+// 	var quill = new Quill( '#' + id, {
+// 		modules: { 
+//             toolbar: true,
+//         },
+// 		theme: 'snow'
+// 	} );
+// 	quill.on( 'text-change', function () {
+// 		// TODO: Sanitize and convert to mediawiki markdown
+//         el.html(quill.container.firstChild.innerHTML);
+// 	} );
+// });
